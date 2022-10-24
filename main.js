a2ff82edd6c3f441d322ce9a49466262daea878a
kim-ki-ryong <rlarlfyd1258@naver.com>
2022-10-24 13:54:20 +0900

    Js.Home transparent

--- main.js
+++ main.js
@@ -13,14 +13,36 @@ document.addEventListener('scroll', () => {
 
 //Handle scrolling when tappng on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
-navbar.addEventListener('click', (event)=> {
+navbarMenu.addEventListener('click', (event)=> {
     
     const target = event.target;
     const link = target.dataset.link;
     if (link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: 'smooth'});
+    scrollIntoView(link);
 });
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact')
+homeContactBtn.addEventListener('click',()=> {
+    scrollIntoView('#contact');
+
+});
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY/homeHeight;
+}), 
+
+
+
+
+
+
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth'});
+}
